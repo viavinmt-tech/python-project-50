ba54cb483e95854068e79df917d47afe8da202e6
vimt <viavinmt@gmail.com>
2026-02-04 20:45:46 +0300

    format

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -3,9 +3,18 @@ import argparse
 
 def main():
     parser = argparse.ArgumentParser(
+        prog='gendiff',
+        usage='gendiff [-h] [-f FORMAT] first_file second_file',
         description='Compares two configuration files and shows a difference.'
     )
-    
+    parser.add_argument(
+        '-f', 
+        '--format',
+        dest='format',
+        metavar='FORMAT',
+        help='set format of output'
+    )
+
     parser.add_argument('first_file')
     parser.add_argument('second_file')
     
