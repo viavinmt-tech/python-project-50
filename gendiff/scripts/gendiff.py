185755bd8c8178e8960d0ea25e8a72b68f048c53
vimt <viavinmt@gmail.com>
2026-02-23 16:00:57 +0300

    update

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,16 +1,17 @@
-import argparse  # NOSONAR
+# NOSONAR
+import argparse
 
 from gendiff import generate_diff
 
 
-def main():  # NOSONAR
-    parser = argparse.ArgumentParser(  # NOSONAR
+def main():
+    parser = argparse.ArgumentParser(
         prog="gendiff",
         usage="gendiff [-h] [-f FORMAT] first_file second_file",
         description="Compares two configuration files and shows a difference.",
     )
 
-    parser.add_argument(  # NOSONAR
+    parser.add_argument(
         "-f",
         "--format",
         dest="format",
@@ -18,18 +19,18 @@ def main():  # NOSONAR
         default="stylish",
         choices=["stylish", "plain", "json"],
         help="set format of output (default: stylish)",
-    )  # NOSONAR
+    )
 
-    parser.add_argument("first_file")  # NOSONAR
-    parser.add_argument("second_file")  # NOSONAR
+    parser.add_argument("first_file")
+    parser.add_argument("second_file")
 
-    args = parser.parse_args()  # NOSONAR
+    args = parser.parse_args()
 
-    diff = generate_diff(  # NOSONAR
-        args.first_file, args.second_file, args.format # NOSONAR
-    )  # NOSONAR
-    print(diff)  # NOSONAR
+    diff = generate_diff(
+        args.first_file, args.second_file, args.format
+    )
+    print(diff)
 
 
-if __name__ == "__main__":  # NOSONAR
-    main()  # NOSONAR
+if __name__ == "__main__":
+    main()
